aac2419b5a6f43b3453261d543117491272ded47
Gabe <gabrielarce3@gmail.com>
2024-01-23 12:07:58 -0800

    edit index.js file

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 //   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 // });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.get('/api', async (req, res) => {
   try {
